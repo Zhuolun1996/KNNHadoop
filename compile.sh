8958cdd06c3905a550fd826c215d7fc177086b44
Zhuolun1996 <zhuolun1996@gmail.com>
2019-12-08 14:50:36 -0500

    fix compile shell

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 export JAVA_HOME=/usr/local/jdk1.8.0_101
 mkdir ./build
-javac -cp "./src/main/java/*:/opt/cloudera/parcels/CDH/lib/hadoop/*:/opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/*" ./src/main/java/*.java -d ./build -Xlint
+javac -cp "./src/main/java/:/opt/cloudera/parcels/CDH/lib/hadoop/*:/opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/*" ./src/main/java/*.java -d ./build -Xlint
 jar cvf knnMapReduce.jar -C build/ .
