560866483698db90d479255813e22b074b61027b
Zhuolun1996 <zhuolun1996@gmail.com>
2019-12-08 12:34:39 -0500

    add output

--- src/main/java/KnnMapReduce.java
+++ src/main/java/KnnMapReduce.java
@@ -13,17 +13,21 @@ public class KnnMapReduce {
         knnConf.set("size", "100");
         knnConf.set("grain", "4");
         knnConf.set("k", args[1]);
+        knnConf.set("fs.defaultFS", "hdfs://ric-master-01.sci.pitt.edu:8020");
         MapReduce1.run(args);
         CellMerger cellMerger = new CellMerger();
-        knnConf.set("fs.defaultFS", "hdfs://ric-master-01.sci.pitt.edu:8020");
         FileSystem hdfsFileSystem = FileSystem.get(knnConf);
         FileSystem localFileSystem = FileSystem.getLocal(new Configuration());
         FileUtil.copyMerge(hdfsFileSystem, new Path("/user/zhl137/knnData/output1/"), localFileSystem, new Path("./knnData/output1/output"), false, knnConf, null);
         HashMap<Integer, HashMap<String, Integer>> cellShape = cellMerger.mergeCell("knnData/output1/output");
         knnConf.set("cellShape", Util.serializeHashMap(cellShape));
+        System.out.println(knnConf.get("cellShape"));
         MapReduce2.run(args);
         MapReduce3.run(args);
         MapReduce4.run(args);
+
+        FileUtil.copyMerge(hdfsFileSystem, new Path("/user/zhl137/knnData/output4/"), localFileSystem, new Path("./knnData/result"), false, knnConf, null);
+
     }
 }
 
