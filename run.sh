36d1ed88e6e33af80b118792b24739e1cade38d0
Zhuolun1996 <zhuolun1996@gmail.com>
2019-12-08 14:57:19 -0500

    fix run

--- run.sh
+++ run.sh
@@ -6,4 +6,7 @@ hadoop fs -rm -r knnData/output1
 hadoop fs -rm -r knnData/output2
 hadoop fs -rm -r knnData/output3
 hadoop fs -rm -r knnData/output4
+hadoop fs -mkdir knnData
+hadoop fs -mkdir knnData/input
+hadoop fs -put ./knnData/input/test.txt knnData/input/
 hadoop jar knnMapReduce.jar KnnMapReduce "$@"
